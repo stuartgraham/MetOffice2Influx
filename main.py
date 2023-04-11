e89ae69a0dd45e3f73a8e5fdb02e643c4b450f74
Stuart Graham <stuart@stuart-graham.com>
2023-04-12 00:47:26 +0100

    Simplify client and harmonise v1 and v2 connectivity

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 import requests
 import schedule
 from icecream import ic
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import SYNCHRONOUS
 
 # GLOBALS
 INFLUX_VERSION = int(os.environ.get("INFLUX_VERSION", 2))
@@ -15,36 +17,25 @@ INFLUX_HOST_PORT = int(os.environ.get("INFLUX_HOST_PORT", ""))
 INFLUX_DATABASE = os.environ.get("INFLUX_DATABASE","")
 INFLUX_BUCKET = os.environ.get("INFLUX_BUCKET", "")
 INFLUX_TOKEN = os.environ.get("INFLUX_TOKEN", "")
-INFLUX_ORG = os.environ.get("INFLUX_ORG", "")
+INFLUX_ORG = os.environ.get("INFLUX_ORG", "-")
 LATITUDE = os.environ.get("LATITUDE", "")
 LONGITUDE = os.environ.get("LONGITUDE", "")
 RUNMINS = int(os.environ.get("RUNMINS", 1))
 LOGGING = bool(os.environ.get("LOGGING", False))
 
+# Logging
 if not LOGGING:
     ic.disable()
 
+# Creates a bucket for backward compat
 if INFLUX_VERSION == 1:
-    from influxdb import InfluxDBClient
-    INFLUX_CLIENT = InfluxDBClient(
-        host=INFLUX_HOST, port=INFLUX_HOST_PORT, database=INFLUX_DATABASE
-        )
+    INFLUX_BUCKET = f"{INFLUX_DATABASE}/autogen"
 
-elif INFLUX_VERSION == 2: 
-    from influxdb_client import InfluxDBClient
-    from influxdb_client.client.write_api import SYNCHRONOUS
-    INFLUX_CLIENT = InfluxDBClient(
-        url=f"http://{INFLUX_HOST}:{INFLUX_HOST_PORT}", org=INFLUX_ORG, token=INFLUX_TOKEN
-        )
-    INFLUX_WRITE_API = INFLUX_CLIENT.write_api(write_options=SYNCHRONOUS)
-
-if INFLUX_VERSION == 99:
-    from influxdb_client import InfluxDBClient
-    from influxdb_client.client.write_api import SYNCHRONOUS
-    INFLUX_CLIENT = InfluxDBClient(
-        url=f"http://{INFLUX_HOST}:{INFLUX_HOST_PORT}", org='-', token=''
-        )
-    INFLUX_WRITE_API = INFLUX_CLIENT.write_api(write_options=SYNCHRONOUS)
+# Instantiate Influx Client
+INFLUX_CLIENT = InfluxDBClient(
+    url=f"http://{INFLUX_HOST}:{INFLUX_HOST_PORT}", org=INFLUX_ORG, token=INFLUX_TOKEN
+    )
+INFLUX_WRITE_API = INFLUX_CLIENT.write_api(write_options=SYNCHRONOUS)
 
 JSON_OUTPUT = "output.json"
 
@@ -81,17 +72,11 @@ def load_weather_data():
 # Determines client type and formats write correctly
 def write_to_influx(data_payload):
     ic(data_payload)
+
     if INFLUX_VERSION == 1:
-        response = INFLUX_CLIENT.write_points([data_payload]) 
-        success = response
-    elif INFLUX_VERSION == 2:
-        response = INFLUX_WRITE_API.write(INFLUX_BUCKET, INFLUX_ORG, data_payload)
-        success = response is None  # In InfluxDB 2.x, a successful write returns None
-    elif INFLUX_VERSION == 99:
-        bucket = f"{INFLUX_DATABASE}/autogen"
-        org = '-'
-        response = INFLUX_WRITE_API.write(bucket, org, data_payload)
-        success = response is None  # In InfluxDB 2.x, a successful write returns None
+        INFLUX_BUCKET = f"{INFLUX_DATABASE}/autogen"
+    response = INFLUX_WRITE_API.write(INFLUX_BUCKET, INFLUX_ORG, data_payload)
+    success = response is None  # In InfluxDB 2.x, a successful write returns None
 
     if success:
         ic("SUCCESS: Data written to InfluxDB")
