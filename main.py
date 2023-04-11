5c8e9c610e87de84a1c538fc350bbe981c30ab7a
Stuart Graham <stuart@stuart-graham.com>
2023-04-12 00:27:21 +0100

    Tweak client params

--- main.py
+++ main.py
@@ -42,8 +42,9 @@ if INFLUX_VERSION == 99:
     from influxdb_client import InfluxDBClient
     from influxdb_client.client.write_api import SYNCHRONOUS
     INFLUX_CLIENT = InfluxDBClient(
-        url=f"http://{INFLUX_HOST}:{INFLUX_HOST_PORT}", database=INFLUX_DATABASE
+        url=f"http://{INFLUX_HOST}:{INFLUX_HOST_PORT}", org='-', token=''
         )
+    INFLUX_WRITE_API = INFLUX_CLIENT.write_api(write_options=SYNCHRONOUS)
 
 JSON_OUTPUT = "output.json"
 
@@ -80,11 +81,16 @@ def load_weather_data():
 # Determines client type and formats write correctly
 def write_to_influx(data_payload):
     ic(data_payload)
-    if INFLUX_VERSION == 1 or INFLUX_VERSION == 99:
+    if INFLUX_VERSION == 1:
         response = INFLUX_CLIENT.write_points([data_payload]) 
         success = response
     elif INFLUX_VERSION == 2:
         response = INFLUX_WRITE_API.write(INFLUX_BUCKET, INFLUX_ORG, data_payload)
+        success = response is None  # In InfluxDB 2.x, a successful write returns None
+    elif INFLUX_VERSION == 99:
+        bucket = f"{INFLUX_BUCKET}/autogen"
+        org = '-'
+        response = INFLUX_WRITE_API.write(bucket, org, data_payload)
         success = response is None  # In InfluxDB 2.x, a successful write returns None
 
     if success:
